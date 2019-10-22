42c07ec4f8197b02faed7b4994bcb59be110dedb
David Cardenas <davidcardenas1993@yahoo.com>
2019-10-22 08:25:47 -0600

    added some psuedo code and created sample array. Also performed quick console.log to make sure array was created correctly

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//I want to dynamically create buttons with each searched value
+//Create an array which houses a few searches for user example
+//Create function for gifs to begin animation and stop animation
+//Create call to gif api
+
+var topics = [
+    "Cars", "Avengers", "Hammer", "Subwoofer", "Indigenous"
+]
+
+console.log(topics);
