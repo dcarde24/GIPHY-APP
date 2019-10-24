8e862f2e27df54401ce3506c5fbb573cba29790d
David Cardenas <davidcardenas1993@yahoo.com>
2019-10-23 20:09:26 -0600

    added for loop function to dynamically create gif buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,22 @@
 //I want to dynamically create buttons with each searched value
 //Create an array which houses a few searches for user example
-//Create function for gifs to begin animation and stop animation
-//Create call to gif api
-
 var topics = [
     "Cars", "Avengers", "Hammer", "Subwoofer", "Indigenous"
 ]
 
-console.log(topics);
+console.log(topics[1]);
+//Create function for gifs to begin animation and stop animation
+//Create call to gif api
+
+function mcGif () {
+for(var i = 0; i < topics.length; i++){
+    $('<button>')
+        .attr('id', topics[i])
+        .text(topics[i])
+        .appendTo((theButtons)) //This did not work initially due to div name being in quotes
+        .click(function(){
+            clicks.push(this.id);
+        });
+}
+};
+mcGif();
