d0c33abf5fb9a1f676102f149259860321446fb0
David Cardenas <davidcardenas1993@yahoo.com>
2019-11-20 11:35:29 -0700

    added ajax call and function for giphy search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,10 +7,10 @@
 //Create call to gif api
 
 $(document).ready(function() {
-
+//added array
     var topics = ["Cars", "Avengers", "Hammer", "Subwoofer", "Indigenous"];
 
-
+//create function for adding buttons and then appending them to an array
     function buttons(arrayUse, addClass, addButton){
         $(addButton).empty();
 
@@ -19,7 +19,49 @@ $(document).ready(function() {
             var a = $("<button>")
             a.addclass(addclass);
             a.attr("data-type", arrayUse[i]);
-            
+            a.text(arrayUse[i]);
+
+            $(addButton).append(a);
+
         }
     }
+//Create a function call to giphy for gif search
+    $(document.onabort("click", ".gifButtons", function() {
+
+        $("#newGifs").empty();
+
+        $(".gifButtons").removeClass("active");
+        $(this).addClass("active");
+
+        var type = $(this).attr("data-type");
+        var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=KA4jqDVANEZo1GtpOp0jo5sY1JVUKcmj";
+
+        $.ajax({
+            url:queryUrl,
+            method: "GET"
+        })
+
+        .then(function(response) {
+            var result = response.data;
+
+            for (var i =0; i < results.length; i++) {
+                var topDiv = $("<div class=\"topic\">");
+
+                var rating = result[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                var animated = results[i].images.fixed_height.url;
+                var still = results[i].images.fixed_height.url;
+
+                var gitImage = $("<img>");
+                gitImage.attr("src", still);
+                gitImage.attr("data-still", still);
+                gitImage.attr("data-animate", animated);
+                gitImage.attr("data-state", "still");
+                gitImage.addClass("git-Image");
+            }
+        })
+
+    }))
 })
