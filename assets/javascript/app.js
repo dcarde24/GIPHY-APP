90f6007ac50afd4bd02ab1ee3de63f0f7a625238
David Cardenas <davidcardenas1993@yahoo.com>
2019-11-20 09:31:31 -0700

    After watching class activities, began reworking gif app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,25 @@
 //I want to dynamically create buttons with each searched value
 //Create an array which houses a few searches for user example
-var topics = [
-    "Cars", "Avengers", "Hammer", "Subwoofer", "Indigenous"
-]
-
-console.log(topics[1]);
+// var topics = [
+//     "Cars", "Avengers", "Hammer", "Subwoofer", "Indigenous"
+// ]
 //Create function for gifs to begin animation and stop animation
 //Create call to gif api
 
-function mcGif () {
-for(var i = 0; i < topics.length; i++){
-    $('<button>')
-        .attr('id', topics[i])
-        .text(topics[i])
-        .appendTo((theButtons)) //This did not work initially due to div name being in quotes
-        .click(function(){
-            clicks.push(this.id);
-        });
-}
-};
-mcGif();
+$(document).ready(function() {
+
+    var topics = ["Cars", "Avengers", "Hammer", "Subwoofer", "Indigenous"];
+
+
+    function buttons(arrayUse, addClass, addButton){
+        $(addButton).empty();
+
+        for (var i = 0; i < arrayUse.length; i++){
+
+            var a = $("<button>")
+            a.addclass(addclass);
+            a.attr("data-type", arrayUse[i]);
+            
+        }
+    }
+})
